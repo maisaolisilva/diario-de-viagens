61e5c307a83736ac40ab1570d9989b09394f0412
Maisa Oliveira <maisaolisilva@gmail.com>
2025-01-13 14:49:21 -0300

    tag meta viewport adicionada ao layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
+      <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>

310437f5a3937472a365bfc4e5148abea4b91db8
Maisa Oliveira <maisaolisilva@gmail.com>
2025-01-13 14:54:25 -0300

    css do card para telas pequenas marcado com !important

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}

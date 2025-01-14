d4515ac06b69dfddaed9f4f068dcc9c85f7a62f4
Maisa Oliveira <maisaolisilva@gmail.com>
2025-01-14 20:59:39 -0300

    correção de erro nos caminhos das páginas no menu de navegação

--- app/componentes/Menu.tsx
+++ app/componentes/Menu.tsx
@@ -1,21 +1,19 @@
+"use client";
+
 import styles from "./menu.module.css";
 import Link from "next/link";
 
-const NavigationMenu = ({}) => (
-  <div className="dropdown">
-    <button className={styles.botao} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-      Minhas Viagens
-    </button>
-    <ul className="dropdown-menu">
-      <li><Link className="dropdown-item" href="/">Home</Link></li>
-      <li><Link className="dropdown-item" href="/viagens/santiago">Santiago</Link></li>
-      <li><Link className="dropdown-item" href="/viagens/macae">Macaé</Link></li>
-    </ul>
-  </div>
-);
-
 export default function Menu() {
-  if (!process.browser) return null; // Only render on client-side
-
-  return <NavigationMenu />;
+  return (
+    <div className="dropdown">
+      <button className={styles.botao} type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Minhas Viagens
+      </button>
+      <ul className="dropdown-menu">
+        
+        <li><Link className="dropdown-item" href="/viagens/santiago">Santiago</Link></li>
+        <li><Link className="dropdown-item" href="/viagens/macae">Macaé</Link></li>
+      </ul>
+    </div>
+  );
 }

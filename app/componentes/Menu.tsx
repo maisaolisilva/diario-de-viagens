484271da6db00e55c67ca1316cf5c03f9b292ade
Maisa Oliveira <maisaolisilva@gmail.com>
2025-01-14 20:43:14 -0300

    correção de erro nos caminhos das páginas no menu de navegação

--- app/componentes/Menu.tsx
+++ app/componentes/Menu.tsx
@@ -1,18 +1,23 @@
 "use client";
 
 import styles from "./menu.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Menu() {
+
+  //hook para não gerar erros nos caminhos dos links do menu em ambiente de produção
+  const pathname = usePathname();
+
   return (
     <div className="dropdown">
-  <button className={styles.botao} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Minhas Viagens
-  </button>
-  <ul className="dropdown-menu">
-    <li><a className="dropdown-item" href="/">Home</a></li>
-    <li><a className="dropdown-item" href="/viagens/santiago">Santiago</a></li>
-    <li><a className="dropdown-item" href="/viagens/macae">Macaé</a></li>
-  </ul>
-</div>
+      <button className={styles.botao} type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Minhas Viagens
+      </button>
+      <ul className="dropdown-menu">
+        <li><a className="dropdown-item" href={pathname === "/" ? "#" : "/"}>Home</a></li>
+        <li><a className="dropdown-item" href={pathname === "/viagens/santiago" ? "#" : "/viagens/santiago"}>Santiago</a></li>
+        <li><a className="dropdown-item" href={pathname === "/viagens/macae" ? "#" : "/viagens/macae"}>Macaé</a></li>
+      </ul>
+    </div>
   );
 }

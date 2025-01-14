8928de813c61ff30ec430a7c87764a67efbf1c2d
Maisa Oliveira <maisaolisilva@gmail.com>
2025-01-14 20:52:08 -0300

    correção de erro nos caminhos das páginas no menu de navegação

--- app/componentes/Menu.tsx
+++ app/componentes/Menu.tsx
@@ -1,9 +1,21 @@
 "use client";
 
 import styles from "./menu.module.css";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Menu() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Isso só será executado no cliente
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null; // Retorna null durante a renderização do servidor
+  }
+
   return (
     <div className="dropdown">
       <button className={styles.botao} type="button" data-bs-toggle="dropdown" aria-expanded="false">

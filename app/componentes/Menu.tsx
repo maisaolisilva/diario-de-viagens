2f06db216adc26ca2f9a2dddd6871453140ba12e
Maisa Oliveira <maisaolisilva@gmail.com>
2025-01-14 21:14:41 -0300

    correção de erro nos caminhos das páginas no menu de navegação

--- app/componentes/Menu.tsx
+++ app/componentes/Menu.tsx
@@ -1,18 +1,26 @@
-"use client";
 
 import styles from "./menu.module.css";
-import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Menu() {
+  const [pathname, setPathname] = useState("");
+
+  //garante que o código que acessa o document seja acessado apenas ao lado do cliente já que document não é disponível do lado do servidor
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setPathname(window.location.pathname);
+    }
+  }, []);
+
   return (
     <div className="dropdown">
       <button className={styles.botao} type="button" data-bs-toggle="dropdown" aria-expanded="false">
         Minhas Viagens
       </button>
       <ul className="dropdown-menu">
-        
-        <li><Link className="dropdown-item" href="/viagens/santiago">Santiago</Link></li>
-        <li><Link className="dropdown-item" href="/viagens/macae">Macaé</Link></li>
+        <li><a className="dropdown-item" href={pathname === "/" ? "#" : "/"}>Home</a></li>
+        <li><a className="dropdown-item" href={pathname === "/viagens/santiago" ? "#" : "/viagens/santiago"}>Santiago</a></li>
+        <li><a className="dropdown-item" href={pathname === "/viagens/macae" ? "#" : "/viagens/macae"}>Macaé</a></li>
       </ul>
     </div>
   );

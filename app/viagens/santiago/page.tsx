954756a15a986c648271584e1a5d0a90df5b3c03
Maisa Oliveira <maisaolisilva@gmail.com>
2025-02-02 16:37:56 -0300

    criado um componente com useEffect para o script do google adsense para que o script seja carregado apensa quando a página estiver carregada

--- app/viagens/santiago/page.tsx
+++ app/viagens/santiago/page.tsx
@@ -2,7 +2,6 @@
 
 import Card from "@/app/componentes/Card";
 import styles from "../page.module.css";
-import Script from 'next/script';
 import Link from "next/link";
 import AdSense from "@/app/componentes/AdSense";
 
